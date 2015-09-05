2d71dd138f4f77ff408a7b319bbdc8b735f779cb
Ansel Halliburton <github@anseljh.com>
2015-09-05 00:12:27 -0700

    More conventional code style; passes JSHint

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ var expressions = [
   /two \(2\)/,
   /three \(3\)/,
   /four \(4\)/,
-  /five \(5\)/ ]
+  /five \(5\)/
+];
 
 function message(form, path, expression, match) {
-  var word = match[0]
+  var word = match[0];
   return {
     message: '"' + word + '" repeats a written number and numeral, which is redundant and error-prone',
     level: "info",
     path: path,
     source: 'doubleplus-numbers',
-    url: null } }
+    url: null
+  };
+}
 
-var annotator = require('commonform-regexp-annotator')(expressions, message)
+var annotator = require('commonform-regexp-annotator')(expressions, message);
 
-module.exports = annotator
+module.exports = annotator;
