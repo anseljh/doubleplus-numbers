55bb849ed0cb06018d667b903f9b50fcd650a825
Kyle Mitchell <kyle@kemitchell.com>
2015-09-09 00:30:25 -0700

    Remove extra code from build script

--- build.js
+++ build.js
@@ -2,12 +2,6 @@ var range = require('array-range');
 var numberStringRepresentation = require('number-string-representation');
 var RANGEMAX = 100;
 
-// Regex for detecting numerals:
-//    \d+       First numerals
-//    (,\d+)*   Optionally, a comma and some more numerals (may be repeated)
-//    (\.\d+)?  Optionally, a decimal point followed by more numerals
-var reNumerals = /\d+(,\d+)*(\.\d+)?/i;
-
 function deDollarize(dollary) {
   // Unfortunately, number-string-representation alawys returns stuff about
   // dollars at the end of its output. This function trims it off.
@@ -19,37 +13,6 @@ function deDollarize(dollary) {
   } else {
     return dollary;
   }
-}
-
-function getAllNumerals(input) {
-  /*
-    returns a list of all numerals (as strings) in the input string
-
-    good test string: "one 234.56 three 456 five 67.89 foo"
-
-    more test strings:
-      two (2) things
-      three (3) more
-      five dollars and twenty cents (5.20)!
-      four thousand (4,000)!!!
-      some more (1,234.567)
-      billions and billions (1,234,567,890.10)
-      one 234.56 three 456 five 67.89 foo
-
-    Test at http://regexpal.com/
-  */
-
-  r = [];
-  var matches = input.match(reNumerals);
-  console.log("reNumerals: " + reNumerals);
-  console.log("matches: " + matches);
-  for (i=0; i < matches.length; i++) {
-    var match = matches[i];
-    console.log("i=" + i + "; match: " + match);
-    r.push(match);
-    console.log("r: " +r);
-  }
-  return r;
 }
 
 function getNumberWords(input) {
